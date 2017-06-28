0b67101a42ef264926ae3571e9a5cdeefb9bfff3
savaki <matt.ho@gmail.com>
2017-06-28 10:54:24 -0700

    - added examples with single and multiple services

---
+++ examples/multiple/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/altairsix/nats-proxy"
+	"github.com/nats-io/go-nats"
+)
+
+func Service(label string) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, label)
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	nc, _ := nats.Connect(nats.DefaultURL)
+
+	// Start Gateway
+	//
+	gw, _ := nats_proxy.NewGateway(
+		nats_proxy.WithSubject("api"),
+		nats_proxy.WithNats(nc),
+	)
+	server := &http.Server{
+		Addr:    ":7070",
+		Handler: gw,
+	}
+	go server.ListenAndServe()
+
+	// Start Service Foo
+	//
+	foo, _ := nats_proxy.Wrap(Service("foo"),
+		nats_proxy.WithSubject("api.foo"), // e.g. /foo*
+		nats_proxy.WithNats(nc),
+	)
+	doneFoo, _ := foo.Subscribe(ctx)
+
+	// Start Service Bar
+	//
+	bar, _ := nats_proxy.Wrap(Service("bar"),
+		nats_proxy.WithSubject("api.bar"), // e.g. /bar*
+		nats_proxy.WithNats(nc),
+	)
+	doneBar, _ := bar.Subscribe(ctx)
+
+	// Make Request
+	//
+	resp, _ := http.Get("http://localhost:7070/foo")
+	io.Copy(os.Stdout, resp.Body)
+	io.WriteString(os.Stdout, "\n")
+
+	resp, _ = http.Get("http://localhost:7070/bar")
+	io.Copy(os.Stdout, resp.Body)
+	io.WriteString(os.Stdout, "\n")
+
+	cancel()
+	<-doneFoo
+	<-doneBar
+}
---
+++ examples/simple/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/altairsix/nats-proxy"
+	"github.com/nats-io/go-nats"
+)
+
+func Hello(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "hello world")
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	nc, _ := nats.Connect(nats.DefaultURL)
+
+	// Start Gateway
+	//
+	gw, _ := nats_proxy.NewGateway(nats_proxy.WithNats(nc))
+	server := &http.Server{
+		Addr:    ":7070",
+		Handler: gw,
+	}
+	go server.ListenAndServe()
+
+	// Start Service
+	//
+	r, _ := nats_proxy.Wrap(http.HandlerFunc(Hello),
+		nats_proxy.WithNats(nc),
+	)
+	done, _ := r.Subscribe(ctx)
+
+	// Make Request
+	//
+	resp, _ := http.Get("http://localhost:7070/")
+	io.Copy(os.Stdout, resp.Body)
+
+	cancel()
+	<-done
+}
